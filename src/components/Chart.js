854b89f2bdb93a0101a16760df2efdc6737a4ad1
Michael Vakoc <mivakoc@microsoft.com>
2017-11-04 15:04:13 +0100

    chart v2

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,23 +1,34 @@
 import React, {Component} from 'react'
-import {Line, LineChart, ResponsiveContainer} from 'recharts'
+import {Line, LineChart, ResponsiveContainer, XAxis, CartesianGrid} from 'recharts'
 
 const data = [
-  {name: '11', uv: 3000, pv: 2400, amt: 3000},
-  {name: '12', uv: 3000, pv: 1398, amt: 2210},
-  {name: '1', uv: 2000, pv: 9800, amt: 2290},
-  {name: '2', uv: 2780, pv: 3908, amt: 3000},
-  
-
+  {name: '1', uv: 3000, pv: 3000},
+  {name: '2', uv: 3000, pv: 3000},
+  {name: '3', uv: 3200, pv: 3200},
+  {name: '4', uv: 3100, pv: 3100},
+  {name: '5', uv: 3230, pv: 3230},
+  {name: '6', uv: 3200, pv: 2400},
+  {name: '7', uv: 3280, pv: 2350},
+  {name: '8', uv: 3450, pv: 2350},
+  {name: '9', uv: 3400, pv: 2200},
+  {name: '10', uv: 3600, pv: 2200},
+  {name: '11', uv: 3500, pv: 2100}
 ];
 
 export default class Chart extends Component {
     render () {
       return (
-          <ResponsiveContainer width={"100%"} height={80}>
+        <div>
+          <span className="b purple ma1">-</span><span className="f7">Aktuální období (4.5.2017 - 3.11.2018)</span>
+          <ResponsiveContainer width={"100%"} height={200}>
         <LineChart data={data}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <Line type="natural" dataKey="uv" stroke="#8884d8" dot={false}/>
+            <Line type="natural" dataKey="pv" stroke="#000" dot={false}/>
+            <XAxis dataKey="name" />
+            <CartesianGrid vertical={false}/>
         </LineChart>
         </ResponsiveContainer>
+        </div>
       )
     }
   }
