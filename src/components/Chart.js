31d805577b93b185577a79297c6b2ea639849506
Michael Vakoc <mivakoc@microsoft.com>
2017-11-04 15:24:13 +0100

    chart v3

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
-import {Line, LineChart, ResponsiveContainer, XAxis, CartesianGrid} from 'recharts'
+import {Line, LineChart, ResponsiveContainer, XAxis, CartesianGrid, ReferenceLine, YAxis} from 'recharts'
 
 const data = [
   {name: '1', uv: 3000, pv: 3000},
   {name: '2', uv: 3000, pv: 3000},
   {name: '3', uv: 3200, pv: 3200},
-  {name: '4', uv: 3100, pv: 3100},
-  {name: '5', uv: 3230, pv: 3230},
-  {name: '6', uv: 3200, pv: 2400},
+  {name: '4', uv: 3250, pv: 2900},
+  {name: '5', uv: 3280, pv: 2730},
+  {name: '6', uv: 3300, pv: 2400},
   {name: '7', uv: 3280, pv: 2350},
   {name: '8', uv: 3450, pv: 2350},
   {name: '9', uv: 3400, pv: 2200},
@@ -18,14 +18,16 @@ const data = [
 export default class Chart extends Component {
     render () {
       return (
-        <div>
+        <div className="pt3">
           <span className="b purple ma1">-</span><span className="f7">Aktuální období (4.5.2017 - 3.11.2018)</span>
           <ResponsiveContainer width={"100%"} height={200}>
         <LineChart data={data}>
-            <Line type="natural" dataKey="uv" stroke="#8884d8" dot={false}/>
-            <Line type="natural" dataKey="pv" stroke="#000" dot={false}/>
+            <Line animationEasing="ease-out" type="monotone" dataKey="uv" stroke="#8884d8" dot={false}/>
+            <Line animationEasing="ease-out" type="monotone" dataKey="pv" stroke="#000" dot={false}/>
             <XAxis dataKey="name" />
+            <YAxis hide={true} domain={[1500, 'dataMax + 500']}/>
             <CartesianGrid vertical={false}/>
+            <ReferenceLine x="3" stroke="purple"/>
         </LineChart>
         </ResponsiveContainer>
         </div>
