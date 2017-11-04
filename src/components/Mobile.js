92c85a3a47810ef5c3de1dd0f14144e4a8388514
Rene Brandel <renebrandel@Renes-MacBook-Pro.local>
2017-11-04 11:13:37 +0100

    setup router

---
+++ src/components/Mobile.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Mobile extends Component {
+  render () {
+    return (
+      <div>
+        tihs is the mobile app
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Mobile from './components/Mobile';
 import registerServiceWorker from './registerServiceWorker';
+import {Router, Route, browserHistory } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const NotFound = () => (
+  <div>
+    404 Not found
+  </div>
+)
+
+
+ReactDOM.render( (
+  <Router history={browserHistory}>
+    <Route path='/' component={Mobile}/>
+    <Route path='/dashboard' component={App}/>
+    <Route path='*' component={NotFound}/>
+</Router> ),
+  document.getElementById('root')
+);
 registerServiceWorker();
