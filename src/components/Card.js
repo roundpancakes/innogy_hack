321e2420f4f3593d7690c02ffb7a9bd7897d19b8
Rene Brandel <renebrandel@Renes-MacBook-Pro.local>
2017-11-04 17:51:23 +0100

    Carousel is in!

---
+++ src/components/Card.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div className="flex flex-column items-center">
+        <div
+          className="w-100"
+          style={{
+            height: 100,
+            backgroundImage: `url('${this.props.item.path}')`,
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            backgroundRepeat: 'no-repeat'
+          }}
+        />
+        <div className="flex items-center justify-center mt1">
+          {this.props.item.name}
+        </div>
+        <div className="w-50">
+        <div className='flex justify-between items-center self-center mt2 w-100'>
+          <div className="f7">
+            Price:
+          </div>
+          <div className="f6 b ml3 flex items-center">
+            {this.props.item.price}
+          </div>
+        </div>
+        <div className='flex justify-between items-center self-center mt1 w-100'>
+          <div className="f7">
+            Savings:
+          </div>
+          <div className="f6 b ml3 flex items-center">
+            {this.props.item.savings}
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
